f7e4ea0fa3f6840cee9b5f6bc0bf6fb4336e75fd
Prokopenko S.A <serg-prokopenko@mail.ru>
2021-01-21 19:50:13 +0300

    Edit ProjectStudy21

--- ProjectStudy21/src/ExceptionMain.java
+++ ProjectStudy21/src/ExceptionMain.java
@@ -1,23 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import MyException.*;
 
 public class ExceptionMain {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String line = reader.readLine();
-        System.out.println(methodOne(line));
+    public static void main(String[] args) {
+        String line = "23f";
+        int numberOverHundred = 500;
+        int numberLessHundred = 50;
+        methodOne(line);
+
+        try {
+            myCheckedException(numberOverHundred);
+        } catch (NumberIsOverHundredException e) {
+            System.out.println(e);
+        }
+
+        try {
+            myUncheckedException(numberLessHundred);
+        } catch (NumberIsLessHundredException e) {
+            System.out.println(e);
+        }
     }
 
     public static int methodOne(String line) {
         try {
             int number = Integer.parseInt(line);
             return number;
-        } catch (Exception e) {
+        } catch (NumberFormatException e) {
             System.out.println(e);
-            return 0;
+        }
+        return 0;
+    }
+
+    public static void myCheckedException(int number) throws NumberIsOverHundredException {
+        if (number <= 100) {
+            System.out.println("Число: " + number);
+        } else {
+            throw new NumberIsOverHundredException("Число: " + number + " больше 100");
         }
     }
 
+    public static void myUncheckedException(int number) throws NumberIsLessHundredException {
+        if (number >= 100) {
+            System.out.println("Число: " + number);
+        } else {
+            throw new NumberIsLessHundredException("Число: " + number + " меньше 100");
+        }
+    }
 }
