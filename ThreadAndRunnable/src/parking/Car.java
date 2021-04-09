29b046f4a58197278e2ea8073bed871e50143452
Prokopenko S.A <serg-prokopenko@mail.ru>
2021-04-09 23:24:07 +0300

    Edit Parking for ThreandAndRunnadble

--- ThreadAndRunnable/src/parking/Car.java
+++ ThreadAndRunnable/src/parking/Car.java
@@ -13,25 +13,25 @@ public class Car extends Thread{
             try {
                 System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " заезжает на парковку");
                 Thread.sleep(100);
-                System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " стоит на парковке");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             AutoParking.entry.remove(Thread.currentThread());
         }
-        synchronized (AutoParking.autoParking) {
-            AutoParking.autoParking.add(Thread.currentThread());
-            try {
-                Thread.sleep(300);
-                System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " выезжает с парковки");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            AutoParking.autoParking.remove(Thread.currentThread());
+
+        AutoParking.autoParking.add(Thread.currentThread());
+        try {
+            System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " стоит на парковке");
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        AutoParking.autoParking.remove(Thread.currentThread());
+
         synchronized (AutoParking.departure) {
             AutoParking.departure.add(Thread.currentThread());
             try {
+                System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " выезжает с парковки");
                 Thread.sleep(100);
                 System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " уехал");
             } catch (InterruptedException e) {
