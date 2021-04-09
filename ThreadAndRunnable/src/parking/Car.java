241af0bc8c65d28ab62fa58f556a35afebbbaf7d
Prokopenko S.A <serg-prokopenko@mail.ru>
2021-04-09 23:26:58 +0300

    Edit Parking for ThreandAndRunnadble

--- ThreadAndRunnable/src/parking/Car.java
+++ ThreadAndRunnable/src/parking/Car.java
@@ -9,35 +9,35 @@ public class Car extends Thread{
     @Override
     public void run() {
         synchronized (AutoParking.entry) {
-            AutoParking.entry.add(Thread.currentThread());
             try {
+                AutoParking.entry.add(Thread.currentThread());
                 System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " заезжает на парковку");
                 Thread.sleep(100);
+                AutoParking.entry.remove(Thread.currentThread());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            AutoParking.entry.remove(Thread.currentThread());
         }
 
-        AutoParking.autoParking.add(Thread.currentThread());
         try {
+            AutoParking.autoParking.add(Thread.currentThread());
             System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " стоит на парковке");
             Thread.sleep(300);
+            AutoParking.autoParking.remove(Thread.currentThread());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        AutoParking.autoParking.remove(Thread.currentThread());
 
         synchronized (AutoParking.departure) {
-            AutoParking.departure.add(Thread.currentThread());
             try {
+                AutoParking.departure.add(Thread.currentThread());
                 System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " выезжает с парковки");
                 Thread.sleep(100);
                 System.out.println("Автомобиль под номером " + Thread.currentThread().getName() + " уехал");
+                AutoParking.departure.remove(Thread.currentThread());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            AutoParking.departure.remove(Thread.currentThread());
         }
     }
 }
